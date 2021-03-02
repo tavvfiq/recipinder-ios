5fd33522b6d4768ed2941a2dbd84dcb706b54ae5
tavvfiq <taufiqwidinugroho@gmail.com>
2021-03-02 16:35:18 +0700

    Initial Commit

---
+++ recipinder/recipinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipinderApp.swift
+//  recipinder
+//
+//  Created by Taufiq Widi on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct recipinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
